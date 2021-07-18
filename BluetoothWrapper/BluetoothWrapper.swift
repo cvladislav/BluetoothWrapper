ab4ece5daa4c32f58f45e293de9afa22e36d321e
cvl <cvl@ciklum.com>
2021-07-18 13:10:18 +0300

    Add stub implementation + Tests

---
+++ BluetoothWrapper/BluetoothWrapper.swift
@@ -0,0 +1,13 @@
+public final class BluetoothWrapper {
+
+    let name = "BluetoothWrapper"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
