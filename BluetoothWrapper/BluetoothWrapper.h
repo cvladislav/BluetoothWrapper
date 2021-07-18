7a1cdbe73d6aaef11de365b0e7738d9a046e6413
cvl <cvl@ciklum.com>
2021-07-18 12:16:34 +0300

    Initial Commit

---
+++ BluetoothWrapper/BluetoothWrapper.h
@@ -0,0 +1,18 @@
+//
+//  BluetoothWrapper.h
+//  BluetoothWrapper
+//
+//  Created by User on 18.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BluetoothWrapper.
+FOUNDATION_EXPORT double BluetoothWrapperVersionNumber;
+
+//! Project version string for BluetoothWrapper.
+FOUNDATION_EXPORT const unsigned char BluetoothWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BluetoothWrapper/PublicHeader.h>
+
+
