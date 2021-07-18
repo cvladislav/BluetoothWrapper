7dbc81eab6e1bef5afd62456fab7d78bb81497c7
cvl <cvl@ciklum.com>
2021-07-18 12:32:08 +0300

    general - Add examples

---
+++ BluetoothWrapperExamples/BluetoothWrapperExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothWrapperExamplesApp.swift
+//  BluetoothWrapperExamples
+//
+//  Created by User on 18.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothWrapperExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
